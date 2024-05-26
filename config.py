143f97bc5e74a280c2aac7ed06db4c05e0f0b7f0
Likhon Sheikh <156697347+likhonsible@users.noreply.github.com>
2024-05-26 10:34:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 {
-    "api_id": xxxxx, // Put your API ID here
-    "api_hash": "xxxxx", // Put your API Hash here
-    "admin": xxxxx, // Put your Admin Telegram user ID here
+    "api_id": 20017510,
+    "api_hash": "a0e64c41b5c06e032c080f7dcc4863dc",
+    "admin": 7102036745, // Put your Admin Telegram user ID here
     "auto_upgrade": true, // Set to true to enable auto-upgrade
     "max_charge_level": 1, // Set maximum charge level
     "max_energy_level": 1, // Set maximum energy level
